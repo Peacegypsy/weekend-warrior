3f13ab151205bd7b9c2feaa030cd5c66506669b9
Xtina <2006peacegypsy@gmail.com>
2018-09-26 12:09:33 -0700

    still sorting out structure. two tests passing, ten skips

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -4,24 +4,28 @@ class Alouette
   def self.lines_for_verse(verse_num)
     bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
     i = verse_num - 1
-    # each_bird_part = []
+    @each_bird_part = []
 
-    return "Et " + "#{bird_parts[i]}" + "!"
-      # each_bird_part << line.to_s
+    line = "Et " + "#{bird_parts[i]}" + "!"
+      @each_bird_part << line.to_s
 
   end
 
   def self.verse(verse_num)
-    i = verse_num
+    i = verse_num - 1
 
-    puts "Je te plumerai #{lines_for_verse(i).split(' ')[1..-1].join(' ')}"
+    puts "Je te plumerai #{@each_bird_part[i].split(' ')[1..-1].join(' ')}"
     i += 1
   end
 
   def self.sing
-    opening = ("Alouette, gentille alouette, Alouette, je te plumerai.")
-    three_lines = ("Alouette!\nAlouette!\nA-a-a-ah")
-    puts opening
-    puts three_lines
+    oc_part = ("Alouette, gentille alouette, Alouette, je te plumerai.\nAlouette!\nAlouette!\nA-a-a-ah" )
+
+      puts oc_part
+      puts "Je te plumerai #{@each_bird_part}"
+      puts verse*2
+      puts oc_part
+
+
   end
 end
