172b3f6fd5a988ddee926a201a89dc92d3eeda24
Xtina <2006peacegypsy@gmail.com>
2018-09-26 11:18:58 -0700

    got bird part selected and opening lines set

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,27 @@
 class Alouette
+
+
   def self.lines_for_verse(verse_num)
+    bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+    i = verse_num - 1
+    each_bird_part = []
+
+      line =   "Et " + "#{bird_parts[i]}" + "!"
+      each_bird_part << line.to_s
+  
   end
 
   def self.verse(verse_num)
+    i = verse_num - 1
+
+    puts "Je te plumerai #{lines_for_verse(i)}"
+    i += 1
   end
 
   def self.sing
+    opening = ("Alouette, gentille alouette, Alouette, je te plumerai.")
+    three_lines = ("Alouette!\nAlouette!\nA-a-a-ah")
+    puts opening
+    puts three_lines
   end
 end
