e70b999f27afd998c3dc8e6c8be78b0099ce5bf1
Xtina <2006peacegypsy@gmail.com>
2018-09-26 23:43:14 -0700

    changed lines_for_verse to loop in a loop

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,30 +1,34 @@
 class Alouette
 
-
+  @bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
   def self.lines_for_verse(verse_num)
-    bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
     i = verse_num - 1
-    @each_bird_part = []
-
-    line = "Et " + "#{bird_parts[i]}" + "!"
-      @each_bird_part << line.to_s
-
-  end
+    verse_num.times do
+      @bird_parts.each do |part|
+        puts "Je te plumerai #{@bird_parts[i]}\n"*2
+        puts ("Et " + "#{@bird_parts[i]}" + "!\n")*2
+      end
+    end
 
   def self.verse(verse_num)
+    # Je te plumerai la tête.
+    # Je te plumerai la tête.
+    # Et la tête!
+    # Et la tête!
     i = verse_num - 1
-
-    puts "Je te plumerai #{@each_bird_part[i].split(' ')[1..-1].join(' ')}"
+    puts "Je te plumerai #{bird_part[i].split(' ')[1..-1].join(' ')}"
     i += 1
   end
 
   def self.sing
-    oc_part = ("Alouette, gentille alouette, Alouette, je te plumerai.\nAlouette!\nAlouette!\nA-a-a-ah" )
+    opening = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+    end_part = ("Alouette, gentille alouette, Alouette, je te plumerai.\nAlouette!\nAlouette!\nA-a-a-ah" )
 
-      puts oc_part
+    puts opening
       puts "Je te plumerai #{@each_bird_part}"
       puts verse*2
-      puts oc_part
+      puts end_part
 
 
   end
