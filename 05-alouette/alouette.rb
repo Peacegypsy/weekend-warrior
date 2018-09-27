6d843455d67738e58015102580554f4f4d1ad68b
Xtina <2006peacegypsy@gmail.com>
2018-09-27 00:29:32 -0700

    checked that lines_for_verse array is array of strings, which it is, but is failing test

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,15 +1,15 @@
 class Alouette
 
   @bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
   def self.lines_for_verse(verse_num)
 
     i = verse_num - 1
     j = 1
     verse_num.times do
-        puts "Je te plumerai #{@bird_parts[j]}\n"*2
-        puts ("Et " + "#{@bird_parts[j]}" + "!\n")*2
-        j += 1
-      end
+      # puts "Je te plumerai #{@bird_parts[j]}\n"*2
+      puts ("Et " + "#{@bird_parts[j]}" + "!\n")*2
+      j += 1
     end
   end
   def self.verse(verse_num)
