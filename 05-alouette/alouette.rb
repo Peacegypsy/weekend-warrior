51dd369cd28c51cf58ff886eb7d0dab484208dfd
Xtina <2006peacegypsy@gmail.com>
2018-09-27 14:00:06 -0700

    testing out verse and lines for verse methods

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,35 +1,34 @@
 class Alouette
 
   @bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
-
+  @bird_part = []
   def self.lines_for_verse(verse_num)
 
     i = verse_num - 1
     j = 1
     verse_num.times do
-      # puts "Je te plumerai #{@bird_parts[j]}\n"*2
-      puts ("Et " + "#{@bird_parts[j]}" + "!\n")*2
+     ("Et " + "#{@bird_parts[j]}" + "!\n")*2
       j += 1
     end
   end
+
   def self.verse(verse_num)
-    # Je te plumerai la tête.
-    # Je te plumerai la tête.
-    # Et la tête!
-    # Et la tête!
     i = verse_num - 1
-    puts "Je te plumerai #{bird_part[i].split(' ')[1..-1].join(' ')}"
-    i += 1
+    verse_num.times do
+      puts "Je te plumerai #{@bird_parts[i].split(' ')[1..-1].join(' ')}"*2
+      puts "#{@bird_parts[i]}"*2
+      i += 1
+    end
   end
 
   def self.sing
-    opening = "Alouette, gentille alouette,\nAlouette, je te plumerai."
-    end_part = ("Alouette, gentille alouette, Alouette, je te plumerai.\nAlouette!\nAlouette!\nA-a-a-ah" )
+    opening = "Alouette, gentille alouette,\nAlouette, je te plumerai.\n"
+
+    end_part = ("Alouette, gentille alouette, Alouette, je te plumerai.\nAlouette!\nAlouette!\nA-a-a-ah\n" )
 
     puts opening
-      puts "Je te plumerai #{@each_bird_part}"
-      puts verse*2
-      puts end_part
+    puts verse(num)
+    puts end_part
 
 
   end
