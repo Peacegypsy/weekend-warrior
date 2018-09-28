4e5282097a34e7289b6abbf7ef5c759aed6d2184
Xtina <2006peacegypsy@gmail.com>
2018-09-27 19:50:13 -0700

    two tests failed, not sure why because it works in IRB?

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -2,23 +2,29 @@ class Alouette
 
   @bird_parts = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
   @bird_part = []
-  def self.lines_for_verse(verse_num)
 
-    i = verse_num - 1
-    j = i
-    verse_num.times do
-      puts "Et #{@bird_parts[j]}!\n"*2
-      j -= 1
+  def self.lines_for_verse(verse_num)
+    k = verse_num
+    if k == 0
+      puts "Et #{@bird_parts[0]}!\n"*2
+      puts "Alouette!\nAlouette!\nA-a-a-ah\n"
+    elsif k >= 1
+      j = k - 1
+      verse_num.times do
+        puts "Et #{@bird_parts[j]}!\n"*2
+        j -= 1
+      end
+      puts "Alouette!\nAlouette!\nA-a-a-ah\n"
     end
-    puts "Alouette!\nAlouette!\nA-a-a-ah\n"
+
   end
 
   def self.verse(verse_num)
     i = verse_num - 1
-    k = verse_num
+
     puts "Je te plumerai #{@bird_parts[i]}\n"*2
 
-    puts lines_for_verse(k)
+    puts lines_for_verse(i)
 
 
   end
